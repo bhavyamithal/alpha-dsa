d240c4f496d86c3cacc39c8abae6c4b82390f7a9
bhavyamithal <bm820@snu.edu.in>
2023-06-22 13:06:11 +0530

    total cost with gst

--- 1-variables_and_data_types/Assignment.java
+++ 1-variables_and_data_types/Assignment.java
@@ -3,7 +3,14 @@ import java.util.*;
 public class Assignment {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        float a = sc.nextFloat();
-        System.out.println(a * a);
+        System.out.print("Cost of pencil: ");
+        float c1 = sc.nextFloat();
+        System.out.print("Cost of pen: ");
+        float c2 = sc.nextFloat();
+        System.out.print("Cost of eraser: ");
+        float c3 = sc.nextFloat();
+        float total_cost = c1 + c2 + c3;
+        float gst = 0.18f * total_cost;
+        System.out.println(total_cost + gst);
     }
 }
