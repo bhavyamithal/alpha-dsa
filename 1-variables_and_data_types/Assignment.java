2efd74c46e464044b036ce78bd2cd247389224b8
bhavyamithal <bm820@snu.edu.in>
2023-06-22 13:00:23 +0530

    average pf 3 nums

--- 1-variables_and_data_types/Assignment.java
+++ 1-variables_and_data_types/Assignment.java
@@ -3,10 +3,7 @@ import java.util.*;
 public class Assignment {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter 3 numbers: ");
         float a = sc.nextFloat();
-        float b = sc.nextFloat();
-        float c = sc.nextFloat();
-        System.out.println((a + b + c) / 3);
+        System.out.println(a * a);
     }
 }
