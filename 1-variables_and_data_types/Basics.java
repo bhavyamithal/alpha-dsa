748a8f0bcd2329fde6374dbea9727115e7fcda8d
bhavyamithal <bm820@snu.edu.in>
2023-06-22 12:57:19 +0530

    average pf 3 nums

--- 1-variables_and_data_types/Basics.java
+++ 1-variables_and_data_types/Basics.java
@@ -3,9 +3,7 @@ import java.util.*;
 public class Basics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter radius : ");
-        float a = sc.nextFloat();
-        float area = 3.14f * a * a;
-        System.out.println(area);
+        float a = 25.999f;
+        int b = (int) a;
     }
 }
