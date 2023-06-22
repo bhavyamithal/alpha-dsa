40f46ab57e15e016890597dd258a6e06b1bc1cea
bhavyamithal <bm820@snu.edu.in>
2023-06-22 12:08:02 +0530

    initial commit

---
+++ 1-variables&data_types/Basics.java
@@ -0,0 +1,8 @@
+public class Basics {
+    public static void main(String[] args) {
+        int a = 10;
+        String b = "hi";
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
